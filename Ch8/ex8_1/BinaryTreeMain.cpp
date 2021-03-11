0480389108438bb4bee03b83c555eb097bd20c6a
ljw-LAB <su5099@naver.com>
2021-03-11 13:28:00 +0900

    Ch8 Commit  - ex8_1, ex8_2, test_ch8_1 add

---
+++ Ch8/ex8_1/BinaryTreeMain.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "BinaryTree.h"
+
+void InorderTraverse(BTreeNode* bt)
+{
+	if(bt == NULL)
+        return;
+    
+    InorderTraverse(bt->left);
+    printf("%d \n", bt->data);
+    InorderTraverse(bt->right);
+}
+int main(void)
+{
+    BTreeNode *bt1 = MakeBTreeNode();
+    BTreeNode *bt2 = MakeBTreeNode();
+    BTreeNode *bt3 = MakeBTreeNode();
+    BTreeNode *bt4 = MakeBTreeNode();
+
+    SetData(bt1, 1);
+    SetData(bt2, 2);
+    SetData(bt3, 3);
+    SetData(bt4, 4);
+
+    MakeLeftSubTree(bt1, bt2);
+    MakeRightSubTree(bt1, bt3);
+    MakeLeftSubTree(bt2, bt4);
+
+    InorderTraverse(bt1);
+    //printf("%d \n", GetData(GetLeftSubTree(bt1)));
+    //printf("%d \n", GetData(GetLeftSubTree(GetLeftSubTree(bt1))));
+
+    return 0;
+}
