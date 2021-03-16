8fa5d15a61c87e72e31c2ff2aa796dfe6319222f
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-03-16 22:39:51 +0900

    Ch10 Commit  - ex10_5 Modify, test_ch10_1 add

--- Ch10/ex10_5/MergeSort.cpp
+++ Ch10/ex10_5/MergeSort.cpp
@@ -13,9 +13,15 @@ void MergeTwoArea(int arr[], int left, int mid, int right)
     while(fIdx <= mid && rIdx <= right)
     {
         if(arr[fIdx] <= arr[rIdx])
-            sortArr[sIdx] = arr[fIdx++];
+        {
+            //sortArr[sIdx] = arr[fIdx++];
+            printf("1. sortArr[%d] = %d, arr[%d] = %d, %d  \n",sIdx, sortArr[sIdx], fIdx, arr[fIdx], sortArr[sIdx] = arr[fIdx++]);
+        }
         else
+        {
             sortArr[sIdx] = arr[rIdx++];
+            //printf("2. sortArr[%d] = %d, arr[%d] = %d, %d \n",sIdx, sortArr[sIdx], rIdx, arr[rIdx], sortArr[sIdx] = arr[rIdx++] );
+        }
     
         sIdx++;
     }
@@ -23,16 +29,29 @@ void MergeTwoArea(int arr[], int left, int mid, int right)
     if(fIdx > mid)
     {
         for(i=rIdx; i <= right ; i++, sIdx++)
+        {
             sortArr[sIdx] = arr[i];
+            //printf("3. sortArr[%d] = %d, arr[%d] = %d \n",sIdx, sortArr[sIdx], i, arr[i]);
+
+        }
+
     }
     else
     {
         for(i=fIdx; i <= mid; i++, sIdx++)
+        {
             sortArr[sIdx] = arr[i];
+            //printf("4. sortArr[%d] = %d, arr[%d] = %d \n",sIdx, sortArr[sIdx], i, arr[i]);
+        }
+
     }
 
     for(i = left; i <=right ; i++)
+    {
         arr[i] = sortArr[i];
+        //printf("5. arr[%d] = %d, sortArr[%d] = %d \n", i, arr[i], i, sortArr[i]);
+
+    }
     
     free(sortArr);
 }
