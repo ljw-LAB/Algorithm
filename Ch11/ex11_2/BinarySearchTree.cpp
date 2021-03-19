791a53786b493b3e56fd49fd7e5a15199ae6b616
ljw-LAB <su5099@naver.com>
2021-03-19 17:38:02 +0900

    Ch11 Commit  - ex11_1, ex11_2  add

---
+++ Ch11/ex11_2/BinarySearchTree.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "BinaryTree2.h"
+#include "BinarySearchTree.h"
+
+void BSTMakeAndInit(BTreeNode** pRoot)
+{
+    *pRoot = NULL;
+}
+
+BSTData BSTGetNodeData(BTreeNode* bst)
+{
+    return GetData(bst);
+}
+
+void BSTInsert(BTreeNode** pRoot, BSTData data)
+{
+    
+}
+
+BTreeNode BSTSearch(BTreeNode* bst, BSTData target)
+{
+    
+}
