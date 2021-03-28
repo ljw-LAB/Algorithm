6c62391386c6be209537eafef4008245011d4209
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-03-28 23:42:17 +0900

    Ch14 Commit  - ex14_1 add

---
+++ Ch14/ex14_1/DLinkedList.h
@@ -0,0 +1,38 @@
+#ifndef __D_LINKED_LIST_H__
+#define __D_LINKED_LIST_H__
+
+typedef int LData;
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _node
+{
+	LData data;
+	struct _node *next;
+}Node;
+
+typedef struct __linkedList
+{
+	Node *head;
+	Node *cur;
+	Node *before;
+	int numOfData;
+	int(*comp)(LData d1, LData d2);
+
+}LinkedList;
+
+typedef LinkedList List;
+
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
+
+int LFirst(List *plist, LData *pdata);
+int LNext(List *plist, LData *pdata);
+
+LData LRemove(List *plist);
+int LCount(List *plist);
+
+void SetSortRule(List *plist, int(*comp)(LData d1, LData d2));
+
+#endif
